d9b971b67030b18f5a3f2d83f0820e711aee3402
Maolypm <maolyjpm@gmail.com>
2026-07-23 16:27:59 -0300

    Implement Login Page Object Model

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,5 +1,9 @@
 from playwright.sync_api import Page
+from pages.login_page import LoginPage
 
-def test_homepage(page: Page):
-    page.goto("https://www.saucedemo.com/")
-    assert page.title() == "Swag Labs"
+def test_valid_login(page: Page):
+    login_page = LoginPage(page)
+    login_page.navigate()
+    login_page.login("standard_user", "secret_sauce")
+
+    assert "inventory" in page.url
