90a643894c0482c736b525efee6d8fcdc641657a
Maolypm <maolyjpm@gmail.com>
2026-07-24 11:51:16 -0300

    Add automatic screenshots on test failure

--- conftest.py
+++ conftest.py
@@ -2,5 +2,18 @@ import pytest
 from pages.login_page import LoginPage
 
 @pytest.fixture
-def login_page(page):
-    return LoginPage(page)
+def login_page(page, request):
+    login_page = LoginPage(page)
+    yield login_page
+
+    if (request.node.rep_call.failed):
+        page.screenshot(
+            path=f"screenshots/{request.node.name}.png"
+        )
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    report = outcome.get_result()
+
+    setattr(item, "rep_" + report.when, report)
